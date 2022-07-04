6fd60034bd5f44fec2b2d51e75749d40f1c15a0c
Kateryna Podopryhora <kateryna@funda.nl>
2022-07-04 12:02:47 +0200

    Added missing $

--- dotnet-package-v2/template/src/${{ values.package_name }}/ServiceCollectionExtensions.cs
+++ dotnet-package-v2/template/src/${{ values.package_name }}/ServiceCollectionExtensions.cs
@@ -8,7 +8,7 @@ namespace ${{values.package_name}}
     public static class ServiceCollectionExtensions
     {
         /// <summary>
-        /// Registers the dependencies of the {{values.package_name}} into the dependency injection framework.
+        /// Registers the dependencies of the ${{values.package_name}} into the dependency injection framework.
         /// </summary>
         /// <param name="services">The service collection.</param>
         /// <returns>The <see cref="IServiceCollection"/> for further configuration. </returns>
